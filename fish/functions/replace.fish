b557b7086896b1339717ad8fa6760ff4b67b2ba8
Arne Beer <arne@twobeer.de>
2014-06-24 03:27:03 +0200

    Configs updated, playlists updated new function

--- fish/functions/replace.fish
+++ fish/functions/replace.fish
@@ -1,7 +1,7 @@
 function replace
-	set sedcommand 's/'$argv[1]'/'$argv[2]'/g'
+    set sedcommand 's/'$argv[1]'/'$argv[2]'/g'
     set places (grep -rl $argv[1])
     command echo 'Replacing '$argv[1]' by '$argv[2]
     command echo 'in the following Files: '$places
-	command sed -i $sedcommand $places
+    command sed -i $sedcommand $places
 end
---
+++ fish/functions/screenshot.fish
@@ -0,0 +1,5 @@
+function screenshot
+	while true
+        scrot -d $argv[1] -q 100
+    end
+end
