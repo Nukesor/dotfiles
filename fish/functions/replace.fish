3a3c99a786bdd1b5a069930715004f051c778ed0
Arne Beer <arne@twobeer.de>
2014-08-31 00:34:57 +0200

    Replace output fixed

--- fish/functions/replace.fish
+++ fish/functions/replace.fish
@@ -1,7 +1,7 @@
 function replace
-    set sedcommand 's/'$argv[1]'/'$argv[2]'/g'
+	set sedcommand 's/'$argv[1]'/'$argv[2]'/g'
     set places (grep -rl $argv[1])
-    command echo 'Replacing '$argv[1]' by '$argv[2]
-    command echo 'in the following Files: '$places
+    command echo 'Replacing '$argv[1]' by '$argv[2] 'in the following Files: '
+    command echo $places
     command sed -i $sedcommand $places
 end
