531d07c0f55c70e9fbba4df1f2fdfaa47aa2b94c
Arne Beer <arne@twobeer.de>
2015-01-24 13:27:05 +0100

    wscreenshot fixed

--- scripts/.scripts/wscreenshot.sh
+++ scripts/.scripts/wscreenshot.sh
@@ -3,7 +3,7 @@
 while true; do
     i3-msg -t "get_outputs" > temp
     string='"current_workspace":"'$2'"'
-    grep -r $string > /dev/null
+    grep -r -I -q $string
     if [[ $? -eq 0 ]] ; then
         scrot -q 100
     else
