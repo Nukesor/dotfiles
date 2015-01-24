f188cc548b0c25ed91a454a36d698467bc45b9a6
Arne Beer <arne@twobeer.de>
2015-01-24 11:43:11 +0100

    Script output fixed

--- scripts/.scripts/replace.sh
+++ scripts/.scripts/replace.sh
@@ -6,6 +6,6 @@ SEDCOMMAND+='/'
 SEDCOMMAND+=$2
 SEDCOMMAND+='/g'
 echo 'Replacing '$1' by '$2 'in the following Files: '
-echo $PLACES
+echo $(grep --binary-files=without-match -rl $1)
 echo $SEDCOMMAND
 sed -i $SEDCOMMAND $(grep --binary-files=without-match -rl $1)
--- scripts/.scripts/wscreenshot.sh
+++ scripts/.scripts/wscreenshot.sh
@@ -3,13 +3,12 @@
 while true; do
     i3-msg -t "get_outputs" > temp
     string='"current_workspace":"'$2'"'
-    grep -r $string
-    temp=$?
-    rm temp
-    if [[ temp -eq 0 ]] ; then
+    grep -r $string > /dev/null
+    if [[ $? -eq 0 ]] ; then
         scrot -q 100
     else
         echo "Not on specified Workspace"
     fi
     sleep $1
+    rm temp
 done
