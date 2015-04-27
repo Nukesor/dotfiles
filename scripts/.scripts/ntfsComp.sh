f090ac83ba811bd1d02d065f395555d03f0d7f5f
Arne Beer <arne@twobeer.de>
2015-04-27 10:55:17 +0200

    Pkglists updated ntfsComp update

--- scripts/.scripts/ntfsComp.sh
+++ scripts/.scripts/ntfsComp.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-find ./ -print0 | xargs -0 perl-rename 's{[\\:*?"<>|]}{_}g'
-find ./ -print0 | xargs -0 perl-rename 's{&}{+}g'
+
+if [ $# != 1 ] ; then
+    find ./ -print0 | xargs -0 perl-rename -n 's{[\\:*?"<>|]}{_}g'
+elif [ $1 = "dir" ] ; then
+    find ./ -type d -print0 | xargs -0 perl-rename 's{[\\:*?"<>|]}{_}g'
+else
+    find ./ -print0 | xargs -0 perl-rename 's{[\\:*?"<>|]}{_}g'
+fi
