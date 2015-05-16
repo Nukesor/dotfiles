21962178e0678594fab305aaf241e55cd836bd72
Arne Beer <arne@twobeer.de>
2015-05-17 01:48:03 +0200

    Setup script bugs fixed

--- setup/scripts/advanced.sh
+++ setup/scripts/advanced.sh
@@ -4,7 +4,7 @@
 pacman -Sy base-devel zsh sudo --needed --noconfirm
 chsh -s /usr/bin/zsh
 # Copying etc
-cp -rf ../etc/* /etc/
+cp -rf etc/* /etc/
 # Adding and signing Haskell repository for pacman
 pacman -Syy
 pacman -S yaourt --noconfirm
