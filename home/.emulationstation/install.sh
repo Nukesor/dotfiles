60438f92b46fe01cbbd85a33353e5a5d2ddb863f
Arne Beer <arne@twobeer.de>
2019-05-24 23:08:15 +0200

    Emulation station config

---
+++ home/.emulationstation/install.sh
@@ -0,0 +1,29 @@
+yay -S emulationstation emulationstation-autoscraper --noconfirm
+
+# Emulators:
+sudo pacman -S retroarch libretro-shaders-cg --noconfirm
+
+# retro arch cores
+# Nintendo 3DS:
+sudo pacman -S libretro-citra --noconfirm
+
+# Nintendo DS:
+sudo pacman -S libretro-desmume --noconfirm
+
+# Nintendo Game Boy/Game Boy color:
+sudo pacman -S libretro-gambatte --noconfirm
+
+# Nintendo Game Boy Advance:
+sudo pacman -S libretro-mgba --noconfirm
+
+# Nintendo 64:
+sudo pacman -S libretro-parallel-n64 --noconfirm
+
+# SNES:
+sudo pacman -S libretro-snes9x --noconfirm
+
+# NES:
+sudo pacman -S libretro-nestopia --noconfirm
+
+# PS:
+sudo pacman -S libretro-beetle-psx-hw --noconfirm
