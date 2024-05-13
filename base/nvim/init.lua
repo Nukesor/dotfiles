411b2fcc7ce6ef316187b36ac1765832ca9dd360
Arne Beer <contact@arne.beer>
2024-05-13 16:16:53 +0200

    add: neovim lua init docs

--- base/nvim/init.lua
+++ base/nvim/init.lua
@@ -1,5 +1,10 @@
+-- Load the config module located in `lua/config/init.lua`
+-- This handles all plugin logic and any configuration logic that's a bit more involved.
 require("config")
 
 local vim = vim
+
+-- Load a init.vim style vim configuration.
+-- This contains basic configurations for which Lua would be overkill.
 local vimrc = vim.fn.stdpath("config") .. "/vimrc.vim"
 vim.cmd.source(vimrc)
