33adaec9715b1c2d4d07c773cf429c2863c09f37
Arne Beer <contact@arne.beer>
2024-11-15 23:43:46 +0100

    add(neovim): Telescope proximity sort impl

--- groups/base/nvim/init.lua
+++ groups/base/nvim/init.lua
@@ -85,8 +85,3 @@ vim.keymap.set("i", "F1", function() end)
 -- Load the config module located in `lua/config/init.lua`
 -- This handles all plugin logic and any configuration logic that's a bit more involved.
 require('config')
-
--- Load a init.vim style vim configuration.
--- This contains basic configurations for which Lua would be overkill.
-local vimrc = vim.fn.stdpath('config') .. '/vimrc.vim'
-vim.cmd.source(vimrc)
--- groups/base/nvim/lua/config/navigation.lua
+++ groups/base/nvim/lua/config/navigation.lua
@@ -87,3 +87,79 @@ telescope.setup({
         },
     }
 })
+
+-- Custom proximity search implementation
+local conf = require("telescope.config").values
+local finders = require "telescope.finders"
+local make_entry = require "telescope.make_entry"
+local pickers = require "telescope.pickers"
+local sorters = require "telescope.sorters"
+
+function starts_with(str, prefix)
+    return string.sub(str, 1, string.len(prefix)) == prefix
+end
+
+local function get_relative_buffer_path()
+    local full_path = vim.api.nvim_buf_get_name(0)
+    -- In case one of the following conditions apply, use the cwd.
+    -- - We aren't in a file buffer.
+    -- - The filename is empty.
+    -- - The path isn't absolut.e.
+    if full_path == nil or
+        full_path == '' or
+        not starts_with(full_path, '/') then
+        return '.'
+    end
+
+    -- Return the relative path of the buffer to the current cwd.
+    -- That's what proxmity sort expects.
+    local cwd = vim.fn.getcwd()
+    local relative_path = string.gsub(full_path, cwd .. '/', '')
+    -- Fallback to the cwd if:
+    -- - The path equals the cwd.
+    -- - We're in a neo-tree buffer
+    if relative_path == '' or starts_with(full_path, 'neo-tree') then
+        return '.'
+    end
+    return relative_path
+end
+
+-- A file picker that sorts entries based on the proximity of files relative to
+-- the file path of the current buffer.
+-- Requires the `proximity-sort` and `fd` binaries to be present.
+local proximity_files = function(opts)
+    local path = get_relative_buffer_path()
+    -- print(string.format("Relative proximity path: '%s'", path))
+
+    local live_fd = finders.new_job(
+        function(prompt)
+            if not prompt or prompt == "" then
+                prompt = "."
+            end
+
+            local cmd = {
+                "bash",
+                "-c",
+                string.format('fd --full-path --type f %s . | proximity-sort %s', prompt, path),
+            }
+            -- print("Cmd: ", vim.inspect(cmd))
+            return cmd
+        end,
+        opts.entry_maker or make_entry.gen_from_file(opts),
+        opts.max_results,
+        opts.cwd
+    )
+
+    pickers
+        .new(opts, {
+            prompt_title = "Proximity Files",
+            finder = live_fd,
+            previewer = conf.file_previewer(opts),
+            sorter = sorters.highlighter_only(opts),
+            push_cursor_on_edit = true,
+            __locations_input = true,
+        })
+        :find()
+end
+
+vim.keymap.set("n", "<C-p>", function() proximity_files({}) end)
