4903b224c0517f1bd0e67c86622d0ed6471c4780
Arne Beer <contact@arne.beer>
2024-12-11 00:47:16 +0100

    fix(fish): n function

--- groups/cli/fish/functions/n.fish
+++ groups/cli/fish/functions/n.fish
@@ -1,7 +1,7 @@
 # Open my notes
 function n
     set --local note_path 'notes'
-    if [ "$#" != 0 ]
+    if [ $(count $argv) != 0 ]
         set --local note_path $1
     end
     cd ~/Dokumente/Various/notes/ && vim ~/Dokumente/Various/notes/$note_path.md || true
